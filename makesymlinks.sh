8b3f196630a7edbe085245d4d57e069dd1fec79a
Martin Aasa <martin.aasa@gmail.com>
2017-04-17 00:09:14 +0200

    Added Neofetch

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc bash-it gitconfig"    # list of files/folders to symlink in homedir
+files="bashrc vimrc bash-it gitconfig neofetch"    # list of files/folders to symlink in homedir
 
 ##########
 
