8e638d5d3459ea016cfcab50f3732a75debd991c
Martin Aasa <martin.aasa@gmail.com>
2017-11-11 16:44:01 +0100

    Flyttat dotfiles-katalogen till .dotfiles Hackat lite på custom alias

--- README.markdown
+++ README.markdown
@@ -1,28 +1,28 @@
 Dotfiles
 ========
 This repository includes all of my custom dotfiles.  They should be cloned to
-your home directory so that the path is `~/dotfiles/`.  The included setup
+your home directory so that the path is `~/.dotfiles/`.  The included setup
 script creates symlinks from your home directory to the files which are located
-in `~/dotfiles/`.
+in `~/.dotfiles/`.
 
 The setup script is smart enough to back up your existing dotfiles into a
-`~/dotfiles_old/` directory if you already have any dotfiles of the same name as
+`~/.dotfiles_old/` directory if you already have any dotfiles of the same name as
 the dotfile symlinks being created in your home directory.
 
-It will also clone Bash-it from https://github.com/Bash-it/bash-it as it's my prefered 
+It will also clone Bash-it from https://github.com/Bash-it/bash-it as it's my prefered
 way to handle Bash dotfiles.
 
 So, to recap, the install script will:
 
-1. Back up any existing dotfiles in your home directory to `~/dotfiles_old/`
-2. Create symlinks to the dotfiles in `~/dotfiles/` in your home directory
+1. Back up any existing dotfiles in your home directory to `~/.dotfiles_old/`
+2. Create symlinks to the dotfiles in `~/.dotfiles/` in your home directory
 3. Clone the `Bash-it` repository from Github
 
 Installation
 ------------
 
 ``` bash
-git clone https://github.com/martinaasa/dotfiles ~/dotfiles
-cd ~/dotfiles
+git clone https://github.com/martinaasa/dotfiles ~/.dotfiles
+cd ~/.dotfiles
 ./makesymlinks.sh
 ```
--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,8 +6,8 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
+dir=~/.dotfiles                    # dotfiles directory
+olddir=~/.dotfiles_old             # old dotfiles backup directory
 files="bashrc vimrc bash-it gitconfig neofetch"    # list of files/folders to symlink in homedir
 
 ##########
@@ -24,25 +24,25 @@ echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+  echo "Moving any existing dotfiles from ~ to $olddir"
+  mv ~/.$file $olddir
+  echo "Creating symlink to $file in home directory."
+  ln -s $dir/$file ~/.$file
 done
 
 install_bash_it () {
-    # Clone Bash-it repository from GitHub only if it isn't already present
-    if [[ ! -d $dir/bash-it/ ]]; then
-        git clone --depth=1 https://github.com/Bash-it/bash-it.git
-        chmod +x $dir/bash-it/install.sh
-        /bin/sh $dir/bash-it/install.sh -i -n
-    fi
+  # Clone Bash-it repository from GitHub only if it isn't already present
+  if [[ ! -d $dir/bash-it/ ]]; then
+    git clone --depth=1 https://github.com/Bash-it/bash-it.git
+    chmod +x $dir/bash-it/install.sh
+    /bin/sh $dir/bash-it/install.sh -i -n
+  fi
 }
 
 install_bash_it
 
 # setup custom aliases
 if [[ -d $dir/bash-it/ ]]; then
-    ln -s $dir/aliases $dir/bash-it/aliases/custom.aliases.bash
-    echo "Creating symlink for custom Bash-it aliases."
+  ln -s $dir/aliases $dir/bash-it/aliases/custom.aliases.bash
+  echo "Creating symlink for custom Bash-it aliases."
 fi
